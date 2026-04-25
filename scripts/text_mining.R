888047f0aa2ee97ac68b1c9fcf746af56c2df185
Derek Fernandez-Davies <derekfernandezdavies@usf.edu>
2026-04-24 22:05:02 -0400

    Add files via upload

--- scripts/text_mining.R
+++ scripts/text_mining.R
@@ -1,17 +1,17 @@
-library(tidytext)
-library(tidyverse)
-library(tidyr)
 
-twitter_data <- read.csv("Final-Project/data/Tweets-1.csv")
+twitter_data <- read.csv("Tweets-1.csv")
 
 twitter_tibble <- tibble(twitter_data)
 
 twitter_unnested <- twitter_tibble %>%
   unnest_tokens(word,text)
 
+stop_words <- c("like", "work", "right", "well", "enough", "free", "fine", "tough", "afford", "cheaper", "rapid","leverage")
+stopwords_tibble <- tibble(word = stop_words)
 
 twitter_sentiment <- twitter_unnested %>%
   inner_join(get_sentiments("bing")) %>%
+  anti_join(stopwords_tibble, by="word") %>%
   count(word, sentiment, sort = TRUE)
 
 
@@ -21,7 +21,7 @@ negative <- twitter_sentiment %>%
 
 total_negative <- negative %>%
   count(sentiment, sort = TRUE)
-  
+
 positive <- twitter_sentiment %>%
   group_by(sentiment) %>%
   filter(sentiment == "positive")
